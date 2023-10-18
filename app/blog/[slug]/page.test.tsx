69e1242daa5066db4915c66b4f3c49e8f1319b8c
Thuy Nguyen <vanthuy101102@gmail.com>
2023-10-18 12:43:53 +0700

    add some command

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -1,11 +1,16 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import Page from './page'
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Page from './page';
 
 it('App Router: Works with dynamic route segments', () => {
-  render(<Page params={{ slug: 'Test' }} />)
-  expect(screen.getByRole('heading')).toHaveTextContent('Slug: Test')
-})
+  render(<Page params={{ slug: 'Test' }} />);
+  expect(screen.getByRole('heading')).toHaveTextContent('Slug: Test');
+});
+
+it('Name should be in file', () => {
+  render(<Page params={{ slug: 'Test' }} />);
+  expect(screen.getByRole('note')).toBeInTheDocument();
+});
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,18 @@
 type Params = {
   params: {
-    slug: string
-  }
-}
+    slug: string;
+  };
+};
 
 export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` }
+  return { title: `Post: ${params.slug}` };
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}</h1>
+  return (
+    <section>
+      <h1>Slug: {params.slug}</h1>
+      <h2 role='note'>By Thuy Nguyen</h2>
+    </section>
+  );
 }
